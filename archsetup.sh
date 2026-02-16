f3db7dacde0f54c5752d7d28da052518c001ca51
Blake Payne <56892670+payne1778@users.noreply.github.com>
2026-02-16 13:02:17 -0500

    special setup for rose

--- archsetup.sh
+++ archsetup.sh
@@ -1,17 +1,5 @@
 ## System Set-up
 
-# Network
-sudo iwconfig wlan0 essid $1
-if [[ ping -c 1 8.8.8.8 > /dev/null ]]; then
-    echo "FATAL: Could not connect to the internet"
-    exit 1
-fi
-sudo pacman -S --noconfirm networkmanager
-sudo systemctl enable --now NetworkManager
-if !systemctl is-active NetworkManager; then 
-    echo "ERROR: NetworkManager was not started"
-fi
-
 # Audio
 sudo pacman -S --noconfirm pipewire
 sudo pacman -S --noconfirm wireplumber
@@ -24,7 +12,6 @@ sudo pacman -S --noconfirm pipewire-pulse
 sudo pacman -S --noconfirm pipewire-audio
 
 ## Package Manager Set-up
-cp ./pacman.conf /etc/pacman.conf
 sudo pacman -S --noconfirm man-db
 sudo pacman -S --noconfirm git
 cd ~
@@ -35,11 +22,3 @@ EDITOR=true makepkg -si --noconfirm --needed
 ## Essential Packages
 yay -S --noconfirm zen-browser-bin
 yay -S --noconfirm vesktop-bin
-yay -S --noconfirm fastfetch
-echo "fastfetch" >> .bashrc
-
-## IDEs
-yay -S --noconfirm zed
-yay -S --noconfirm code
-yay -S --noconfirm nvim
-
